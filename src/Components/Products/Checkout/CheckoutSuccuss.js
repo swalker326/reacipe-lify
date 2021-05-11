1b7261b6c857bab9457d8455fe7103de48230afb
Shane <shane@swalker.dev>
2021-05-11 16:35:31 -0500

    file fuckery

---
+++ src/Components/Products/Checkout/CheckoutSuccuss.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const CheckoutSuccuss = () => {
+return(
+   <div className='CheckoutSuccuss' >
+     You checked out, good job dude
+   </div>
+ )
+}
+
+export default CheckoutSuccuss
+
+const styles = {
+
+}
